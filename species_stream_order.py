d132c7e4570d5f453afdd063bbfc8ec2c9ed2733
Nick Santos <nickrsan@users.noreply.github.com>
2019-09-24 20:18:47 -0700

    WIP - Additional streamlining and making functions out of code

--- species_stream_order.py
+++ species_stream_order.py
@@ -1,23 +1,30 @@
 from __future__ import print_function
 import datetime
 import logging
-import geodatabase_tempfile
+import os
 
 import arcpy
 
+import geodatabase_tempfile
 import env_manager
 
 log = logging.getLogger("fish_species_occurrence")
 logging.basicConfig()
 
-log.info('Start\t{}'.format(datetime.datetime.now().time()))
+# Constants and configuration items
+FLOWLINES = r"C:\Users\dsx\Code\belleflopt\data\NHDPlusV2\NHDPlusV2.gdb\NHDFlowline_Network"
+HUC12S = r"C:\Users\dsx\Code\PISCES\data\PISCES_map_data.gdb\HUC12FullState"
+SPECIES_RANGES = r""
+RATE = 0.5  # exponential base for decaying presence probability
+MAX_PROBABILITY = 0.9  # Probability to use for the MIN(Max Stream Order) for each species - decays at RATE from there
+ABOVE_MAX_PROBABILITY = 1  # Probability to use for stream orders larger than MIN(Max Stream Order)
 
 
-def get_max_stream_order_by_huc(nhd_flowline, huc12s,):
+def get_max_stream_order_by_huc(nhd_flowline=FLOWLINES, huc12s=HUC12S,):
     """
        Attaches the maximum stream order in each huc12 to the hucs on a new output layer
     :param nhd_data_gdb_path: Path to the geodatabase that has the NHD flowline features in it
-    :return: None - attaches max stream order to each HUC12
+    :return: geodatabase feature class of huc 12s with max stream order attached
     """
 
     log.info('Set working directory to NHDPlusV2\t{}'.format(datetime.datetime.now().time()))
@@ -30,8 +37,6 @@ def get_max_stream_order_by_huc(nhd_flowline, huc12s,):
                                match_option="HAVE_THEIR_CENTER_IN")
 
     # create layers
-    huc12_layer = "HUC12_layer"
-    arcpy.MakeFeatureLayer_management(huc12s, huc12_layer)
     flowline_layer = "Flowline_layer"
     arcpy.MakeFeatureLayer_management(flowline_features, flowline_layer)
     try:  # start a try block so we can clean up the feature layers no matter whether we exit this block on purpose or with an exception
@@ -47,83 +52,94 @@ def get_max_stream_order_by_huc(nhd_flowline, huc12s,):
         log.info('Join field to HUC12\'s\t{}'.format(datetime.datetime.now().time()))
         arcpy.JoinField_management(flowline_features, "HUC_12", stats_table, "HUC_12", ["MAX_StreamOrde"])
     finally:
-        arcpy.Delete_management(huc12_layer)
         arcpy.Delete_management(flowline_layer)
 
     return flowline_features
 
 
-def build_codeblock():
+def get_min_max_stream_order_for_species(species_range, huc12_layer):
+    """
+        Given a path to a species range and an input feature layer of huc 12s, gets the minimum MAX_StreamOrde in
+        the range
+    :param species_range:
+    :param huc12_layer:
+    :return:
+    """
+    # Create feature layer for fish
+    species_range_name = os.path.split(species_range)[1]
+    fish_layer = species_range_name + "_layer"
+    arcpy.MakeFeatureLayer_management(species_range, fish_layer)
+    # select HUC12's by species occurrence and run (min) stats
+    try:
+        log.info(species_range + ' select by location\t{}'.format(datetime.datetime.now().time()))
+        arcpy.SelectLayerByLocation_management(huc12_layer, "HAVE_THEIR_CENTER_IN", fish_layer, selection_type="NEW_SELECTION")
 
-    # set working directory - fish occurence (2)
-    arcpy.env.workspace = "C:/Users/15303/Documents/CWS_Programming/species_occurence/species_ranges.gdb"
-    arcpy.env.overwriteOutput = True
-    log.info('Set working directory to species ranges\t{}'.format(datetime.datetime.now().time()))
+        stats_table = "Stats_{}".format(species_range_name)
+        log.info(species_range + ' summary statistics\t{}'.format(datetime.datetime.now().time()))
+        arcpy.Statistics_analysis(huc12_layer, stats_table, [["MAX_StreamOrde", "MIN"]])
+    finally:
+        arcpy.Delete_management(fish_layer)
 
-    # creates list of all features classes in working directory (watershed level fish species occurence)
-    log.info('List feature classes\t{}'.format(datetime.datetime.now().time()))
-    features = arcpy.ListFeatureClasses()
+    cursor = arcpy.da.SearchCursor(stats_table, ['MIN_MAX_StreamOrde'])
+    for row in cursor:  # there's only one row, so this method should be OK to get the correct value
+        log.info(row[0])
+        min_stream = row[0]
 
-    # initiate codeblock string
-    codeblock = """def getProbability(species, stream_order, join_count):"""
+    return min_stream
 
-    # finds minimum of maximum stream orders for every species of fish
-    for i in features:
 
-        # Create feature layer for fish
-        fish_layer = i + "_layer"
-        arcpy.MakeFeatureLayer_management(i, fish_layer)
-        # select HUC12's by species occurrence and run (min) stats
-        try:
-            log.info(i + ' select by location\t{}'.format(datetime.datetime.now().time()))
-            arcpy.SelectLayerByLocation_management(huc12_layer, "HAVE_THEIR_CENTER_IN", fish_layer)
+def build_codeblock(huc12s,
+                    species_range_geodatabase=SPECIES_RANGES,
+                    rate=RATE,
+                    max_probability=MAX_PROBABILITY,
+                    above_max_probability=ABOVE_MAX_PROBABILITY):
+    """
+        Builds the code block with the probabilities for each species by stream order that we'll use when we run field
+        calculator.
+    :param huc12s:
+    :param rate:
+    :param max_probability:
+    :param above_max_probability:
+    :param species_range_geodatabase:
+    :return:
+    """
+    huc12_layer = "HUC12_layer"
+    arcpy.MakeFeatureLayer_management(huc12s, huc12_layer)
+    try:
+        # set working directory - fish occurence (2)
+        arcpy.env.workspace = species_range_geodatabase
+        log.info('Set working directory to species ranges\t{}'.format(datetime.datetime.now().time()))
 
-            log.info(i + ' summary statistics\t{}'.format(datetime.datetime.now().time()))
-            arcpy.Statistics_analysis(huc12_layer, "Stats_" + i, [["MAX_StreamOrde", "MIN"]])
-        finally:
-            arcpy.Delete_management(fish_layer)
+        # creates list of all features classes in working directory (watershed level fish species occurence)
+        log.info('List feature classes\t{}'.format(datetime.datetime.now().time()))
+        species_features = arcpy.ListFeatureClasses()
 
-        cursor = arcpy.da.SearchCursor("Stats_" + i, ['MIN_MAX_StreamOrde'])
-        for row in cursor:
-            log.info(row[0])
-            min_stream = row[0]
+        # finds minimum of maximum stream orders for every species of fish
+        species_dict = {}
+        for species in species_features:
 
-        dict = {}
-        stream_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-        iteration = 1
-        rate = 0.5  # exponential base
-        max_probability = 0.9  # initial probability
-        for j in stream_list:  # count down stream orders
-            if j > min_stream:
-                dict[j] = max_probability
-            if j == min_stream:
-                dict[j] = max_probability
-            if j == 1:
-                dict[j] = max_probability * (rate) ** iteration
-                break
-            if j <= min_stream:
-                dict[j] = max_probability * (rate) ** iteration
-                iteration += 1  # if i is less than or equal to min stream order, decrease probability per iteration exponentially
+            min_stream = get_min_max_stream_order_for_species(os.path.join(species_range_geodatabase, species), huc12_layer)
 
-        log.info(dict)  # copy output as plain text
-        string_dict = str(dict)  # string to be concacenated to codeblock
-        codeblock += ("\n" + "\t" + i + "_dict" + "=" + string_dict)  # add dictionary to codeblock
+            individual_dict = {}
+            stream_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+            iteration = 1
+            for j in stream_list:  # count down stream orders
+                if j > min_stream:
+                    individual_dict[j] = above_max_probability
+                if j == min_stream:
+                    individual_dict[j] = max_probability
+                if j <= min_stream:
+                    individual_dict[j] = max_probability * (rate) ** iteration
+                    iteration += 1  # if i is less than or equal to min stream order, decrease probability per iteration exponentially
 
-    # builds dict of dicts
-    codeblock += ("\n" + "\t" + "pdict = {")  # initiate dict of dicts
-    iteration = 0  # keeps track of number of interations
-    for i in features:
-        iteration += 1
-        if iteration != len(features):
-            codeblock += ("\'" + i + "\'" + ":" + i + "_dict" + ",")  # append string with comma at end
-        if iteration == len(features):
-            codeblock += ("\'" + i + "\'" + ":" + i + "_dict" + ",")  # no comma
+            log.info(individual_dict)  # copy output as plain text
+            species_dict[species] = individual_dict
 
-    codeblock += "}"  # close dict of dicts
+        # initiate codeblock string with the species dict outside of the function
+        codeblock = "pdict = {}".format(str(species_dict))
 
-    codeblock += """
-#pdict = {"hardhead" : hardhead_dict, "rainbow_trout": rainbow_trout_dict}
-#if stream segment not in spatial join, set prob to 0    
+        codeblock += """def getProbability(species, stream_order, join_count):
+\t#if stream segment not in spatial join, set prob to 0    
 \tif join_count == 0:
 \t\treturn '0'
 \t\t#if stream order is negative (coastline) or does not exist set prob to "NA"
@@ -133,7 +149,10 @@ def build_codeblock():
 \telse:
 \t\treturn str(pdict[species][stream_order])"""
 
-    log.info(codeblock)
+        log.info(codeblock)
+    finally:
+        arcpy.Delete_management(huc12_layer)
+
     return codeblock
 
 
@@ -153,6 +172,8 @@ def get_species_feature_classes(workspace):
 
 
 if __name__ == "__main__":  # if this code is being executed, instead of imported
+
+    log.info('Start\t{}'.format(datetime.datetime.now().time()))
 
     arcpy.env.workspace = "C:/Users/15303/Documents/CWS_Programming/species_occurence/species_ranges.gdb"
     arcpy.env.overwriteOutput = True
@@ -182,30 +203,8 @@ if __name__ == "__main__":  # if this code is being executed, instead of importe
         #expression to be used to fill probability field, calls probability function
         expression = "getProbability(" + "'" + join_features + "'" + ", " + "!StreamOrde!, !Join_Count!)" #concatenation to pass speices name into codeblock, getProbability(i, !StreamOrde!, !Join_Count!)
 
-
-    #     codeblock = """
-    # def getProbability(species, stream_order, join_count):
-    #     hardhead_dict = {1:10, 2:20, 3:30, 4:40, 5:50, 6:60, 7: 70, 8: 80, 9:90, 10:100}
-    #     rainbow_trout_dict = {1:10, 2:10, 3:10, 4:10, 5:10, 6:10, 7: 10, 8: 10, 9:10, 10:10}
-    #     pdict = {"hardhead" : hardhead_dict, "rainbow_trout": rainbow_trout_dict}
-    #     #if stream segment not in spatial join, set prob to 0
-    #     if join_count == 0:
-    #         return '0'
-    #     #if stream order is negative (coastline) or does not exist set prob to "NA"
-    #     if stream_order is None or stream_order < 0 or str(stream_order) == 'nan':
-    #         return 'N/A'
-    #     #call stream order/probability dictionary
-    #     else:
-    #         return str(pdict[species][stream_order])"""
-
-
-
-
-
-
         #fills out probability field
         arcpy.CalculateField_management(in_table, join_features, expression, "Python_9.3", codeblock)
-
 
         # arcpy.AddIndex_management(out_feature_class, ["COMID"], "idx_COMID", "NON_UNIQUE") #add indexes to spatial join output
         # arcpy.AddIndex_management(out_feature_class, ["COMID", i], "idx_COMID_prob", "NON_UNIQUE")
--- tests.py
+++ tests.py
@@ -3,15 +3,12 @@ import pytest
 import species_stream_order
 import support
 
-flowlines = r"C:\Users\dsx\Code\belleflopt\data\NHDPlusV2\NHDPlusV2.gdb\NHDFlowline_Network"
-huc12s = r"C:\Users\dsx\Code\PISCES\data\PISCES_map_data.gdb\HUC12FullState"
-
-
-def test_max_stream_order_by_huc():
+def test_max_stream_order_by_huc(flowlines=species_stream_order.FLOWLINES, huc12s=species_stream_order.HUC12S):
 	max_stream_order_fc = species_stream_order.get_max_stream_order_by_huc(nhd_flowline=flowlines, huc12s=huc12s)
 
-	where_clause = "HUC_12 in ('180201250701', '180201250702','180201250703','180201250704','180201251002')"
-	max_stream_orders = support.get_attribute_dict(max_stream_order_fc, "HUC_12", "MAX_StreamOrde", where_clause=where_clause)
+	# where clause seemed slow - maybe because of a lack of an index. Let's just pull it all in
+	#where_clause = "HUC_12 in ('180201250701', '180201250702','180201250703','180201250704','180201251002')"
+	max_stream_orders = support.get_attribute_dict(max_stream_order_fc, "HUC_12", "MAX_StreamOrde", where_clause=None)
 
 	# manually collected max stream orders by Nick on 9/24/2019
 	assert max_stream_orders["180201250701"] == 3
@@ -19,3 +16,5 @@ def test_max_stream_order_by_huc():
 	assert max_stream_orders["180201250703"] == 4
 	assert max_stream_orders["180201250704"] == 4
 	assert max_stream_orders["180201251002"] == 5
+
+	assert max_stream_orders["180201630703"] == 7  # outlet to suisun bay from the central valley
